84d00ad5624113628b6d30e5345fd0593c698d1c
Ayşe Şen <92683893+Afthonia@users.noreply.github.com>
2022-03-05 16:53:52 +0300

    Create KDVRate.java
    The program takes a value from you and gives you the value you gave, KDV rate (it depends on the real price), how much money KDV rate is equal to and the price with total of KDV.

---
+++ KDVRate.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class KDVRate {
+    public static void main(String[] args) {
+        double realPrice, kdvRate, kdvPrice, priceWithKDV;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Pleace enter the real price: ");
+        realPrice = input.nextDouble();
+        kdvRate = (realPrice > 0 && realPrice <= 1000) ? 0.18 : 0.08;
+        kdvPrice = realPrice * kdvRate;
+        priceWithKDV = realPrice + kdvPrice;
+
+        System.out.println("The price of the product without KDV is $" + realPrice);
+        System.out.println("The KDV rate is $" + kdvRate);
+        System.out.println("The kdv is $" + kdvPrice);
+        System.out.println("The price of the product with KDV is $" + priceWithKDV);
+    }
+}

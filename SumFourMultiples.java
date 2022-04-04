10e3cf9d2e0dec28c1aec38d7d89ea118399ace3
Ayşe Şen <92683893+Afthonia@users.noreply.github.com>
2022-04-04 11:38:57 +0300

    Add files via upload
    The program will calculate the sum of the given numbers so long as those numbers are even, the multiples of 4 and positive.

---
+++ SumFourMultiples.java
@@ -0,0 +1,23 @@
+package Practices;
+
+import java.util.Scanner;
+
+public class SumFourMultiples {
+    public static void main(String[] args) {
+        int number;
+        int total = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter a number: ");
+            number = input.nextInt();
+
+            if (number % 4 == 0)
+                total += number;
+
+        } while (number % 2 == 0 && number > 0);
+
+        System.out.println(total);
+    }
+}

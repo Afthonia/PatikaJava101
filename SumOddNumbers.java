8ed3e4b7541efe1e64e9efed3f7a277f0eb7775f
Ayşe Şen <92683893+Afthonia@users.noreply.github.com>
2022-04-04 11:12:12 +0300

    Add files via upload
    As long as the positive numbers are still entered, the program will calculate the sum of the odd numbers. When the program stops, it will output the sum.

---
+++ SumOddNumbers.java
@@ -0,0 +1,22 @@
+package Practices;
+
+import java.util.Scanner;
+
+public class SumOddNumbers {
+    public static void main(String[] args) {
+        int num;
+        int sum = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter a number: ");
+            num = scan.nextInt();
+
+            if (num % 2 == 1)
+                sum += num;
+        } while (num>0);
+
+        System.out.println("Total : " + sum);
+    }
+}
